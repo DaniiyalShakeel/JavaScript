5bf3ae3973d816838037aa260ac7254adb01df08
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-11-26 20:21:48 +0000

    variable

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,8 +1,15 @@
 const accountId = 1122334455
 let accountEmail = "daniyalshakeel@gmail.com"
 var accountPassword = "112233"
-accountId = "Multan"
-
+accountCity = "Multan"
+let accountState;
 // accountId = 1424512 // not allowed
 
+accountEmail = "danirajput@gmail.com"
+accountPassword = "2121211"
+accountCity = "Lahore"
+
+// Prefare not to use var because  of issue block scope and functional scope
+
 console.log(accountId);
+console.table ([accountId, accountEmail, accountPassword, accountCity, accountState])
