749653da765b9a1377d9b16daeb90dafc73ca6e6
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-11-26 20:20:44 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code in newer version 
+
+// alert (3+3) we are use nodejs not browser
+
+// console.log( 3 
+    // +
+    //  3 ) code readability should be high
+
+    console.log("Daniyal");
+    
+
+
+    let Name = "Daniyal"
+    let age = 24   
+    let isLoggedIn = false
+
+    // number => 2 to power 53
+    // BigInt => Function
+    // String => ""
+    // Boolean => true/false
+    // null => standalone value
+    // undefined => undefined
+    // Symbol => unique
+
+    console.log(typeof null); // object
+    console.log(typeof undefined); // undefined
+    
