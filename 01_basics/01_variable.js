e04ff275a9b4b9233eb7cd0fd4fee5167b250524
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-11-26 18:42:54 +0000

    01_variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 1122334455
+let accountEmail = "daniyalshakeel@gmail.com"
+var accountPassword = "112233"
+accountId = "Multan"
+
+// accountId = 1424512 // not allowed
+
+console.log(accountId);
