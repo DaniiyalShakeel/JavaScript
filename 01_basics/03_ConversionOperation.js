5a8d23a95eeeb6e63482dbcc90fce94dc65f4a29
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-11-26 21:15:24 +0000

    ConversionOperation

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,28 @@
+let score = "Daniyal"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// 33 => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Daniyal"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// In Boolean
+// 1 => True; 0 => False
+// "" => false; "Daniyal" => true
+
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
