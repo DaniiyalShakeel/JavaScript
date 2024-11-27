7140f2c680311a4c97c18d2b4bc3fda719e5b694
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-11-27 22:12:38 +0000

    math

---
+++ 01_basics/math.js
@@ -0,0 +1,32 @@
+const math = 2
+// console.log(math);
+
+// console.log(Math);
+// console.log(Math.round(4.9));
+// console.log(Math.max(4.9));
+// console.log(Math.min(4.9));
+// console.log(Math.sqrt(9484987412348132465231331321546546546546545644498451984));   this is so crazy 
+
+// console.log(Math.log10(2));
+
+
+;
+console.log(Math.random);
+console.log((Math.random()*6 +1));   
+
+// this mathod use for random number like ludo dice
+console.log(Math.floor(Math.random()*6 +1));   
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random * (max - min)) + min);
+
+
+
+
+
+
+
+
