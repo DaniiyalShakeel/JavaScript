2c17b997471034888dc300f526e90ff83dc08c34
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-11-27 08:31:27 +0000

    Conversion

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -24,5 +24,61 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 55
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+
+// **************************************************Operations***********************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(6**9);
+// console.log(2/2);
+// console.log(2^2);
+// console.log(9%16);
+
+let str1 = "Hello"
+let str2 = " Daniyal"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "1");
+
+// console.log((1+2) * 5 / 9);
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+); => Error
+
+
+let num1, num2, num3
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+
+// Postfix increment
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+
+// Prefix increment
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
 
