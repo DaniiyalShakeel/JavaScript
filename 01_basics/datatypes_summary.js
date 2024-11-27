5ea8d02dae34ad55456e9021181f9a744c734a20
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-11-27 13:47:06 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,53 @@
+ Primitive data type
+7 basic types of premitive
+String, Number, Boolean,  null,  undefined, Symbol, BigInt, 
+
+console.log(score)
+
+
+const score = 100
+let userEmail;
+const score = "10"              =>   String
+const scoreValue = 100          =>   Number
+const isLoggedIn = false        =>   Boolean
+const outsideTemp = null        =>   null
+let userEmail;                  =>   undefined
+
+const id = Symbol('123')
+const newid = Symbol('123')
+console.log(id ===  newid); = false       =>   Symbol
+
+JavaScript, adding an n to the end of a number converts it into a BigInt type.BigInt is a special data type used to handle very large numbers that exceed the limits of the Number type.
+const bigNumber = 5498761986546873617n        => BigInt
+
+
+Reference ( Non Primitive)
+types of Non premitive
+
+Array,Object, Function
+
+Array
+const Youtuber = ["BB ki vines", "carry Minati", "CodeWithHarry"]
+
+Object
+let myObj = {
+ name : "Daniyal",   
+ Age : 24,
+}
+
+Function
+let myFunction = function(){
+    console.log("Hello Daniyal")};
+    
+
+
+
+
+
+let x = 10
+let x = "daniyal"
+console.log(id === newid) 
+
+
+
+https://262.ecma-international.org/#sec-typeof-operator
