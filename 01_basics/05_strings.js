26700e5cac62ba679d9932711188eb6d4d0aba76
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-11-27 20:38:12 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name = "daniyal"
+const secondName = "shakeel"
+
+// console.log(name +  secondName +" rajput"); not recommend
+
+// recommended
+// console.log(`Hello my name is ${name} and my father name is ${secondName} cast ${"rajput"}`);
+
+const gameOn = new String ('Subway Surfer')
+
+// console.log(gameOn.length);
+// console.log(gameOn.valueOf ('3'));
+
+const rehman = "             daniyalshakeel                "
+// console.log(rehman);
+// console.log(rehman.trim());
+
+const url = "http://www.google/com/daniyal-shakeel%200rajput"
+
+// console.log(url.replace('%200', '-'));
+
+// const line = "A-quick-brown-fox-jumps-over-the-lazy-dog"
+// console.log(line.split('-'));
+
+
+const icons = '☃★♲';
+
+console.log(icons.codePointAt(1));
