ff75656395ba5fa2e62f855ee82ba46ec70f1732
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-11-27 21:32:01 +0000

    number

---
+++ 01_basics/06_number.js
@@ -0,0 +1,27 @@
+const score = 100
+// console.log(score);
+
+
+
+const balance = new Number(400.8991)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed());
+
+
+console.log(balance.toPrecision(6));
+
+const newbalance = 119449842181
+console.log(newbalance.toLocaleString('en-rs'));
+
+
+Number.MIN_VALUE
+5e-324
+
+
+Number.MAX_VALUE
+1.7976931348623157e+308
+
+Number.MAX_SAFE_INTEGER
+9007199254740991
