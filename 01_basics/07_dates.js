7a5a1b8c5f8f61912634e94cc9cf585a21f9aceb
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-11-28 12:05:04 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,11 @@
+let myDate  = new Date ()
+// console.log(myDate);
+
+// *************************Date****************
+// console.log(myDate.toLocaleTimeString());
+
+// let createDate = new Date (2024, 11, 25, 5, 3, 19)
+let createDate = new Date (12-31-2023)
+console.log(createDate.toLocaleString());
+
+
