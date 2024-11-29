c0a3db5861d8e150a3bff0f81b88765c0218683b
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-11-29 12:55:21 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,55 @@
+
+
+// function myName() {
+//     console.log("D");
+//     console.log("A");
+//     console.log("N");
+//     console.log("I");
+//     console.log("Y");
+//     console.log("A");
+//     console.log("L");
+// }
+
+// // myName()
+
+// // function addTwoNumber(number1, number2, number3, number4){
+// //     console.log(number1 + number2 + number3 + number4);
+    
+// // }
+// //     const result = addTwoNumber(9, 1, 2, 5)
+    
+
+// // function addTwoNumber(number1, number2, number3, number4){
+// //     let result = number1 + number2 + number3 + number4
+    
+// //     console.log("daniyal");
+// //     return result
+
+// // function addTwoNumber(number1, number2, number3, number4){
+// //     return number1 + number2 + number3 + number4
+    
+
+// const result = addTwoNumber(9, 1, 2, 5)
+
+// // console.log("result:", result);
+
+
+// // console.log("result:", result);
+
+
+// // function addTwoNumber(number1, number2, number3, number4){
+// //     return number1 + number2 + number3 + number4
+// // }
+
+function loginuser (username = "shakeel"){
+    if(!username)
+    console.log("Please enter a username");
+    return
+    
+}
+
+return `${username} just logged in`
+
+
+// console.log(loginuser("daniyal"));
+console.log(loginuser("daniyal"));
