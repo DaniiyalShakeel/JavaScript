321c9b92a97b5928adb3aafa585c445c8636f73e
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-11-29 07:34:41 +0000

    object

---
+++ 02_basics/03_object.js
@@ -0,0 +1,29 @@
+// const tinderuser = new Object ()
+const tinderuser = {}
+
+
+tinderuser.id = "112233abc"
+tinderuser.name =   "daniyal"
+tinderuser.email = "daniyalshakeel@gmail.com"
+tinderuser.IsLoggedIn = false
+
+
+// console.log(tinderuser);
+
+const regularuser = {
+    email: "daniyalshakeel@gmail.com",
+    Customer: {
+        CustomerName: {
+            CustomerFullName: {
+            FirstName: "daniyal",
+            LastName: "Shakeel"
+                 }
+            }
+        }
+    }
+
+
+console.log(regularuser.Customer);
+
+
+
