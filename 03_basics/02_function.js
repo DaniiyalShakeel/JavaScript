1380101550a7014de32c4bf71a31df37c399465f
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-12-01 12:59:27 +0000

    function

--- 03_basics/02_function.js
+++ 03_basics/02_function.js
@@ -1,19 +1,38 @@
-function calculateCartPrice(val1, val2, ...num1) {
+// function calculateCartPrice(val1, val2, ...num1) {
+//     return num1
+// }
+
+// // console.log(
+// //  calculateCartPrice(200,400,500,2000));
+
+//  const user = {
+//     username: "dani",
+//     price: 188
+// }
+
+// function handleObject(newObject) {
+//     console.log(`username is ${newObject.username} and price is ${newObject.price}`);
+
+// }
+
+
+//  handleObject(user);
+
+function calculateCartPrice(...num1){
     return num1
 }
 
-// console.log(
-//  calculateCartPrice(200,400,500,2000));
+// console.log(calculateCartPrice(45,45,12,512));
 
- const user = {
-    username: "dani",
-    price: 188
+
+const user ={
+    username: "daniyal"
+    price: 200
 }
 
-function handleObject(newObject) {
-    console.log(`username is ${newObject.username} and price is ${newObject.price}`);
-
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
 }
 
-
- handleObject(user);
+handleObject (user)
