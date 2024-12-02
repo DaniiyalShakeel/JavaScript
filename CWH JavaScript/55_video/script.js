ebb9719a8e88bd04ac6a703ae19d26f3c22b28f6
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-12-02 13:51:12 +0000

    CWH 55

---
+++ CWH JavaScript/55_video/script.js
@@ -0,0 +1,91 @@
+// console.log("this is to much");
+
+var a = 5;
+// a = a + 1 
+var b = 8;
+var c = 5;
+var d = 2;
+var e = " Daniyal"
+
+// console.log(a+b+c+d+e);
+// console.log(typeof d, typeof e);
+
+
+{
+    var a = 22;
+    // console.log(a);
+    
+}
+
+// console.log(a);
+
+
+var _a = 2;
+var _b = 2;
+// var 55c = 2; not allowed
+
+// console.log(a);
+
+
+// const a = 6;
+// a1 = a1 + 1; not allowed because a1 is const
+
+let r = "daniyal";
+let s = 22  ;
+let t = 9.5 ;
+let u = false;
+let v = undefined;
+let x = null;
+let y = BigInt;
+
+// console.log(r,s,t,u,v,x,y);
+
+// ***********************Premitive************* 
+// daniyal = string
+// 22 =    number  
+// 9.5     = number
+// false   = boolean
+// undefined   = undefined
+// null    =   object
+// BigInt  =   function
+      
+
+// console.log(typeof r,typeof s,typeof t,typeof u,typeof v,typeof x,typeof y);
+
+
+let Detail = {
+    name : "Daniyal",
+    "Full Name": "Daniyal shakeel",
+    age: 24,
+    Email: "daniyal@gmail.com",
+    Pakistani: true
+    
+}
+
+Detail.fathername = "shakeel";
+Detail.fathername = "muhammad shakeel";
+// console.log(typeof Detail);
+
+
+ let Store1={
+    "Store Name": 'Sky Grove',
+    Phone: 92317473570,
+    Score: 999,
+
+ }
+
+
+Store1.Phone = 6548
+Store1.Score = 849847658
+
+
+const person={
+    name: "daniyal",
+    Phone: 92317473570,
+    Score: 999,
+    
+}
+
+person.Phone = 6548
+person.Score = 849847658
+console.log(person.Phone, person.name, person.Score);
