224ed2fb4e11bf1b74c0a35ea3b95b98d871581d
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-12-02 17:12:18 +0000

    57

---
+++ CWH JavaScript/57_video/script.js
@@ -0,0 +1,51 @@
+console.log("I m a Tutorial on Loop");
+
+
+// *****basic for loop****
+
+// let a = 1;
+
+// for (let index = 0; index < 200; index++) {
+
+//     console.log(a + index);
+// }
+
+
+// // *************forin loob*********
+// let obj = {
+//     name: "Daniyal",
+//     role: "Developer",
+//     Company: "Me Khud",
+//     Sallery: "Aukat sy kam"
+// }
+
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(element);
+        
+//     }
+ 
+// *****for of loop****
+// for (const lamba of "daniyal") {
+//     console.log(lamba);
+    
+// }
+
+
+// *****************while loop************
+// must be same for loop but diffrent
+
+// let i = 0;
+// while (i<9) {
+//     console.log(i);
+//     i++;
+    
+// }
+
+// ************do while loop*************
+
+let i = 10;
+do {
+    console.log(i);
+    i++;
+} while (i<10);
