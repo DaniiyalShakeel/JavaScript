fe46249f580e3b81a3f624362bcbf0c02f650ffc
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-12-02 16:03:16 +0000

    56

---
+++ CWH JavaScript/56_video/script.js
@@ -0,0 +1,129 @@
+// console.log("Hello i m conditional tutorial");
+
+
+// let age = 19;
+// let grace = 2;
+
+// if((age-grace)>18){
+//     console.log("You can Drive");
+
+// }
+
+// else{
+//     console.log("You cannot Drive");
+
+// }
+
+// let agse = 26;
+
+// *******Arithmatic Operators*****
+// let age = 7;
+// let grace = 2;
+
+// age += grace
+// console.log(age);
+
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age +- grace);
+// console.log(age ** grace);
+// console.log(age -+ grace);
+// console.log(age /+ grace);
+// console.log(age % grace);
+
+
+// ***********Assignment Operators******
+
+
+// =      
+// +=
+// -=
+// *=
+// /=
+// %=
+
+// ********************Comparision Operation***************
+
+
+// ==
+// !=
+// ===
+// !==
+// >
+// <
+// >=
+// <=
+// // !
+
+// let age = 19;
+// // let grace = 2;
+
+// if(age == 18){
+//     console.log("You can Drive");
+
+// }
+
+// else{
+//     console.log("You cannot Drive");
+
+// }
+
+// ***************logical operators*********
+
+// &&
+// ||
+// !
+
+// let age = 19;
+// // let grace = 2;
+
+// if(age == 18){
+//     console.log("You can Drive");
+
+// }
+
+// else{
+//     console.log("You cannot Drive");
+
+// }
+
+
+// let age = 12;
+
+// if (age === 12){
+//     console.log("You can Drive");
+
+// }
+// else if(age === 12){
+//     console.log("Are your Kidding");
+
+// }
+// else if(age === 12){
+//     console.log("Are your again Kidding");
+
+// }
+
+
+
+
+
+
+
+let a = 8
+let b = 6
+let c = a < b ? (a - b) : (b - a)
+// in simple form 
+
+console.log(c);
+
+
+// translate to:
+// if (a < b) {
+//     let c = a - b
+// }
+
+// else {
+//     let c = b - a
+// }
