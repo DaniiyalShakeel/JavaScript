7a7f74b8fdedde092f30e5d7609fc4dbd953b822
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-12-03 11:42:09 +0000

    58

--- CWH JavaScript/58_video/script.js
+++ CWH JavaScript/58_video/script.js
@@ -1,32 +1,37 @@
 
-// function paro(name) {
-//     console.log("Hey" + name + "you are nice");
-//     console.log("Hey" + name + "you are good");
-//     console.log("Hey" + name + "you are so nice");
-//     console.log("Hey" + name + "you are soo cute");
+// function paro(you,name) {
+//     console.log("Hey" + name + you + "are nice");
+//     console.log("Hey" + name + you + "are good");
+//     console.log("Hey" + name + you + "are so nice");
+//     console.log("Hey" + name + you + "are soo cute");
 
-// // }
+// }
+
+
+// paro(" tum ")
 
 
 // paro(" daniyal ")
 // paro(" zeeshan ")
 // paro(" shaheer ")
 
-function sum(a, b, c) {
-    console.log(a + b);
-    return a + b
-}
-result = sum(4, 4)
-result1 = sum(45, 8123)
-result2 = sum(21, 45)
-result3 = sum(13, 45)
-result4 = sum(78, 994)
-
-console.log("Total number sum is", result);
-console.log("Total number sum is", result1);
-console.log("Total number sum is", result2);
-console.log("Total number sum is", result3);
-console.log("Total number sum is", result4);
 
 
+// function sum(a, b, c) {
+//     console.log(a + b);
+//     return a + b
+// }
+// result = sum(4, 4)
+// result1 = sum(45, 8123)
+// result2 = sum(21, 45)
+// result3 = sum(13, 45)
+// result4 = sum(78, 994)
 
+// console.log("Total number sum is", result);
+// console.log("Total number sum is", result1);
+// console.log("Total number sum is", result2);
+// console.log("Total number sum is", result3);
+// console.log("Total number sum is", result4);
+
+
+ 
