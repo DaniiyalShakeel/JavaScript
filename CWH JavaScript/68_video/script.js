502a7aa481053a7f26425b8845aaa9610c93dbd5
DaniiyalShakeel <daniiyalshakeel@gmail.com>
2024-12-04 16:08:58 +0000

    68

---
+++ CWH JavaScript/68_video/script.js
@@ -0,0 +1,19 @@
+// console.log("daniyal");
+
+// let boxes = document.getElementsByClassName("container")
+
+// boxes[3].style.backgroundColor = "red"
+
+
+document.getElementById("cont").style.backgroundColor = "green"
+document.getElementById("cont1").style.backgroundColor = "green"
+document.getElementById("cont2").style.backgroundColor = "green"
+document.getElementById("cont3").style.backgroundColor = "green"
+document.getElementById("cont4").style.backgroundColor = "green"
+
+document.querySelectorAll(".box").forEach(e =>{
+    e.style.backgroundColor = "orange"
+}
+)
+
+document.querySelector(".box").style.backgroundColor = "red "
